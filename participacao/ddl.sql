1b0d1b840482939830ea9d000c8e6de45e527e4c
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2023-08-12 18:05:02 -0300

    db tuning

--- participacao/ddl.sql
+++ participacao/ddl.sql
@@ -1,10 +1,11 @@
 CREATE TABLE public.pessoas (
-	id UUID PRIMARY KEY,
+	id VARCHAR(36) PRIMARY KEY,
 	apelido VARCHAR(32) UNIQUE NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	nascimento DATE NOT NULL,
 	stack JSONB NULL,
-	busca VARCHAR NOT NULL
+	busca TEXT NOT NULL
 );
 
-CREATE INDEX pessoas_busca_idx ON public.pessoas (busca);
+CREATE EXTENSION pg_trgm;
+CREATE INDEX pessoas_busca_idx ON public.pessoas USING GIN (busca GIN_TRGM_OPS);
